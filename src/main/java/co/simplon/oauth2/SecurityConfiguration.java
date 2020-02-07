87f0d4e00996f94a18120b9ab3dd083caf4838dd
bernard hugueney <gitlab.com@bernard-hugueney.org>
2020-02-07 10:45:11 +0100

    Refactor and simplify the ROLE_ADMIN from github org code

--- src/main/java/co/simplon/oauth2/SecurityConfiguration.java
+++ src/main/java/co/simplon/oauth2/SecurityConfiguration.java
@@ -8,22 +8,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import java.util.List;
-import java.util.Map;
-
-import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
-
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -51,47 +42,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService(WebClient rest) {
-        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
-        return new MyOAuth2UserService(delegate, rest);
-    }
-}
-
-class MyOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-    private OAuth2UserService delegate;
-    private WebClient rest;
-
-    public MyOAuth2UserService(DefaultOAuth2UserService delegate,
-                               WebClient rest) {
-        this.delegate = delegate;
-        this.rest = rest;
-    }
-
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
-        OAuth2User user = delegate.loadUser(request);
-        if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
-            return user;
-        }
-        OAuth2AuthorizedClient client = new OAuth2AuthorizedClient
-                (request.getClientRegistration(), user.getName(), request.getAccessToken());
-        String url = user.getAttribute("organizations_url");
-        List<Map<String, Object>> orgs = rest.get().uri(url)
-                .attributes(oauth2AuthorizedClient(client))
-                .retrieve()
-                .bodyToMono(List.class)
-                .block();
-        System.err.println("orgs:" + orgs);
-        for (Map<String, Object> org : orgs) {
-            System.err.println("Checking org:" + org);
-            if ("simplonco".equals(org.get("login"))) {
-                System.err.println("ADMIN!!!!");
-                return new DefaultOAuth2User(AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER, ROLE_ADMIN"),
-                        user.getAttributes(),"login");
-            }
-        }
-        return new DefaultOAuth2User(AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"),
-                user.getAttributes(),"login");
-
-        //throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in Simplon Team", ""));
+        return new MyOAuth2UserService(rest);
     }
 }
